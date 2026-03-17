1ba72597d5d9869f041ecbe5ab98b323b2b1118a
cyiza22 <127378914+cyiza22@users.noreply.github.com>
2026-03-17 20:41:34 +0200

    Update AssistController.php

--- screening-api/app/Http/Controllers/AssistController.php
+++ screening-api/app/Http/Controllers/AssistController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Services\ChatService;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class AssistController extends Controller
 {
